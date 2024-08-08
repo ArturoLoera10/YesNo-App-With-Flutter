296dde27fa812a7716c73c0a1a14e442f30a3358
ArturoLoera10 <loeraarturo26@gmail.com>
2024-08-08 16:40:02 -0600

    Message bubble designs

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:yes_no_app_two/config/theme/app_theme.dart';
+import 'package:yes_no_app_two/presentation/screens/chat/chat_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -12,17 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'Material App',
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 1).theme(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: Center(
-          child: FilledButton.tonal(
-            onPressed: () {},
-            child: const Text("Click me"),
-          ),
-        ),
-      ),
+      home: const ChatScreen(),
     );
   }
 }
---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app_two/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app_two/presentation/widgets/chat/my_message_bubble.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                "https://th.bing.com/th/id/OIP.gswvzbpa7_JzzAjw7hOVCAHaKX?rs=1&pid=ImgDetMain"),
+          ),
+        ),
+        title: const Text('Chat'),
+        centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                  return (index % 2 == 0)
+                      ? const HerMessageBubble()
+                      : const MyMessageBubble();
+                },
+              ),
+            ),
+            Text('Mundo'),
+          ],
+        ),
+      ),
+    );
+  }
+}
